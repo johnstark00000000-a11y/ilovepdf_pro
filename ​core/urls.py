dff6f6040184ad6f350f6455164fc89bd86cf7c5
johnstark00000000-a11y <johnstark00000000@gmail.com>
2026-09-05 15:21:38 +0530

    Delete ​core directory

--- ​core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('tool/<str:tool_name>/', views.tool_detail, name='tool_detail'),
-    path('process/<str:tool_name>/', views.process_pdf, name='process_pdf'),
-]
